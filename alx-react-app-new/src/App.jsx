4123e96c0774476a749420992a0c775c12e4fd09
SAMAR KHALED <142172579+Samarmar2727@users.noreply.github.com>
2025-07-18 19:38:36 +0300

    add a new component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from "./components/UserProfile"
+import Counter from './components/Counter';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <MainContent /> 
         <Footer />
            <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+           <Counter/>
         </div>
     </>
   )
