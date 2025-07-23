1a555845d15c8dfd5ee4e4105106a70d483113c1
Samar Khaled <skhaled510@gmail.com>
2025-07-24 01:27:53 +0300

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import  RecipeList  from "./components/RecipeList";
+import RecipeList from "./components/RecipeList";
 import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
