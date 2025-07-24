7a574644816042229b6514ce4313d8698ff298b6
Samar Khaled <skhaled510@gmail.com>
2025-07-24 19:29:36 +0300

    update

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,15 @@
 import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ id }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
   const handleDelete = () => {
   const confirmed = window.confirm("Are you sure you want to delete this recipe?");
   if (confirmed) {
     deleteRecipe(id);
+     navigate("/");   
   }
 
   };
